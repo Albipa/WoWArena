6566004a285cf391a3b73ee6769a9e897a6c802e
albipa <albion@paulsson.net>
2022-02-16 15:09:53 +0100

    Fixed character creator

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from resources import player, teammate, enemy
 import names
 import os
 import time
-enemygennumber = 0
 classes = ["Warrior", "Paladin", "Hunter" , "Rouge" , "Priest" , "Shaman" , "Mage" , "Warlock" , "Monk" , "Druid" , "Demon Hunter" , "Death Knight" ]
 races = ["Human", "Dwarf", "Night Elf", "Gnome", "Draenei", "Worgen", "Pandaren", "Void Elf", "Lightforged Draenei", "Dark Iron Dwarf", "Kul Tiran", "mechagnome"]
 teammates = []
@@ -60,13 +59,16 @@ def main():
     loader()
     while True:
         global teamgennumber
+        global enemygennumber
         choice = input("""
-> WoW Arena Simulator 2022
+> WoW Simulator 2022
 
 > 1. 2v2
 > 2. 3v3
 > 3. Battlegrounds
 > 4. Custom Match
+> 5. Raiding
+> 6. Dungeons
 
 > © AP Industries 2022, All rights reserved.
 
@@ -91,21 +93,38 @@ def main():
             break
         else:
             print("Try again")
-    print(teamgennumber)
-    
+    print(f"Team generator number{teamgennumber}")
+    print(f"Enemy generator number{enemygennumber}")
     charactercreator()
 
     teammategenerator()
 
+    enemygenerator()
+
 def teammategenerator():
-    print(teamgennumber)
-    teammatenumber = 0
+    print(f"Team generator number{teamgennumber}")
     for i in range(teamgennumber):
-        teammate = resources.player(random.randint(10000,30000),random.randint(2500,7500),random.choice(classes),random.randint(1,50),names.get_first_name(), random.choice(races))
-        globals()[f"teammate{i}"] = teammate
-        print(f"Health: {teammate.health}\nAttack: {teammate.attackpower}\nClass: {teammate.spec}\nArmor: {teammate.armor}\nName: {teammate.name}\nRace: {teammate.race}")
-        teammates.append(teammate)
-    
+        teammatecurrent = resources.teammate(random.randint(10000,30000),random.randint(2500,7500),random.choice(classes),random.randint(1,50),names.get_first_name(), random.choice(races))
+        print(f"Health: {teammatecurrent.health}\nAttack: {teammatecurrent.attackpower}\nClass: {teammatecurrent.spec}\nArmor: {teammatecurrent.armor}\nName: {teammatecurrent.name}\nRace: {teammatecurrent.race}\n")
+        globals()['teammate%s' % i] = teammatecurrent
+        teammates.append(teammatecurrent)
+    hey = 0
+    for i in teammates:
+        hey += 1
+        print(hey)
+
+def enemygenerator():
+    print(f"Enemy generator number{enemygennumber}")
+    for i in range(enemygennumber):
+        enemycurrent = resources.enemy(random.randint(10000,30000),random.randint(2500,7500),random.choice(classes),random.randint(1,50),names.get_first_name(), random.choice(races))
+        print(f"Health: {enemycurrent.health}\nAttack: {enemycurrent.attackpower}\nClass: {enemycurrent.spec}\nArmor: {enemycurrent.armor}\nName: {enemycurrent.name}\nRace: {enemycurrent.race}\n")
+        globals()['teammate%s' % i] = enemycurrent
+        enemies.append(enemycurrent)
+    heye = 0
+    for i in enemies:
+        heye += 1
+        print(heye)
+
 
 
 
